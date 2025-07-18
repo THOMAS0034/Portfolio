f16cad611bccbb41b3aa4b9a6acaf5da7cdab9e5
RyanEE1234 <intern.software@brainwired.in>
2025-07-19 00:07:31 +0530

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
     return (
         <div className="navbar flex justify-around pt-5 pb-5 items-center border-b border-violet-600">
                 <Link to={"/"}><h1 className='text-3xl font-bold font-serif text-white uppercase hover:text-violet-400 transition delay-100'>Ryan.</h1></Link>
-                <ul className='flex gap-10 text-gray-200 font-semibold text-lg'>
+                <ul className='flex gap-10 text-gray-200 font-semibold text-sm lg:text-lg'>
                   <Link to={"/projects"}><li className='font-serif hover:text-violet-300 hover:scale-105 hover:bg-gray-800 rounded-2xl p-2 transition delay-100 ease-in-out cursor-pointer'> Projects</li></Link>
                   <Link to={"/form"} ><li className='font-serif hover:text-violet-300 hover:scale-105 hover:bg-gray-800 rounded-2xl p-2 transition delay-100 ease-in-out cursor-pointer'>Contact</li></Link>
                 </ul>
